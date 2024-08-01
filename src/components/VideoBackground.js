c27ff49f17708bb036cc5a31aa4b2ab58f095421
Dawsons Geek <157927926+dawsonsgeek@users.noreply.github.com>
2024-07-31 21:28:26 -0400

    Updated readme file to include new features addded

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -3,13 +3,14 @@ import useMovieTrailer from '../hooks/useMovieTrailer';
 
 const VideoBackground = ({movieId}) => {
   const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
-  
+
   useMovieTrailer();
   
     return (
-    <div>
+    <div className="w-screen">
       <iframe
-      src={"https://www.youtube.com/embed/"+trailerVideo?.key} 
+      className='w-screen aspect-video'
+      src={"https://www.youtube.com/embed/"+trailerVideo?.key+"?&autoplay=1&mute=1"} 
       title="YouTube video player" 
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share">
       </iframe>
--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -6,7 +6,8 @@ import { addTrailerVideo } from '../utils/movieSlice';
 const useMovieTrailer = (movieId) => {
     const dispatch = useDispatch();
     const getMovieVideos = async () => {
-          const data = await fetch("https://api.themoviedb.org/3/movie/"+movieId+"/videos?language=en-US", API_OPTIONS);
+          const data = await fetch("https://api.themoviedb.org/3/movie/533535/videos?language=en-US", API_OPTIONS);
+        //   const data = await fetch("https://api.themoviedb.org/3/movie/"+movieId+"/videos?language=en-US", API_OPTIONS);
           const json = await data.json();
         //   console.log(json);
   
